127c6dc85f354832fd7125ad25c022196884a14f
Ziaul Chowdhury <romel99@gmail.com>
2024-03-21 11:49:41 +0100

    Implemented class for data loading from TRV

--- data-engineering/src/data_loader.py
+++ data-engineering/src/data_loader.py
@@ -5,50 +5,60 @@ import requests
 import json
 import datetime as dt
 
-trv_api_key = 'adeac1acb7834c50a49f9710c3607625'
-
-def date_minus_now(num_days):
-    ''' Subtracts num_days from current date '''
-
-    date_time_x_days = dt.datetime.now() + dt.timedelta(days=num_days)
-    date_days = str(date_time_x_days.date())
-    return date_days
-
-def get_train_announcement_trv(api_key=''):
-    ''' Gets train announce data from TRV '''
-
-    url_trv = 'https://api.trafikinfo.trafikverket.se/v2/data.json'
-    day_to_request = date_minus_now(-1)
+class DataLoader:
     
-    xml_request = '''
-        <REQUEST>
-            <LOGIN authenticationkey="{0}" />
-            <QUERY objecttype="TrainAnnouncement" schemaversion="1.8">
-                <FILTER>
-                    <EQ name="InformationOwner" value="SJ" />
-                    <EQ name="Advertised" value="true" />
-                    <EQ name='DepartureDateOTN' value="{1}" />
-                </FILTER>
-            </QUERY>
-        </REQUEST>'''.format(api_key, day_to_request)
+    def __init__(self, api_key, num_days):
+        self.api_key = api_key
+        self.num_days = num_days
     
-    print(f'Request data: {xml_request}')
-    headers = {'Content-Type': 'text/xml'}
+    def date_minus_now(self):
+        ''' Subtracts num_days from current date '''
     
-    print('Retrieving train announcement from TRV API ...')
-    response = requests.post(url_trv, data=xml_request, headers=headers)
+        date_time_x_days = dt.datetime.now() + dt.timedelta(days=-self.num_days)
+        date_days = str(date_time_x_days.date())
+        return date_days
     
-    response_code = response.status_code
-    if response.status_code >= 200 and response.status_code < 300:
-        print(f'Valid Response received! Code: {response_code}')
-        response_json = response.json()
-        train_announcements = response_json.get('RESPONSE').get('RESULT')[0].get('TrainAnnouncement')
-        return train_announcements
-    else:
-        print(f'Invalid response received! Response Code: {response_code}')
-        response.raise_for_status()
+    def get_train_announcement_trv(self):
+        ''' Gets train announce data from TRV '''
+    
+        url_trv = 'https://api.trafikinfo.trafikverket.se/v2/data.json'
+        day_to_request = self.date_minus_now()
+                
+        xml_request = '''
+            <REQUEST>
+                <LOGIN authenticationkey="{0}" />
+                <QUERY objecttype="TrainAnnouncement" schemaversion="1.8">
+                    <FILTER>
+                        <EQ name="InformationOwner" value="SJ" />
+                        <EQ name="Advertised" value="true" />
+                        <EQ name='DepartureDateOTN' value="{1}" />
+                        <EQ name="ActivityType"	value="Ankomst" />
+                       
+                    </FILTER>
+                </QUERY>
+            </REQUEST>'''.format(self.api_key, day_to_request)
+        
+        print(f'Request data: {xml_request}')
+        headers = {'Content-Type': 'text/xml'}
+        
+        print('Retrieving train announcement from TRV API ...')
+        response = requests.post(url_trv, data=xml_request, headers=headers)
+        
+        response_code = response.status_code
+        if response.status_code >= 200 and response.status_code < 300:
+            print(f'Valid Response received! Code: {response_code}')
+            response_json = response.json()
+            train_announcements = response_json.get('RESPONSE').get('RESULT')[0].get('TrainAnnouncement')
+            return train_announcements
+        else:
+            print(f'Invalid response received! Response Code: {response_code}')
+            response.raise_for_status()
         
 if __name__ == '__main__':
-    train_announcements = get_train_announcement_trv(trv_api_key)
+    
+    trv_api_key = 'adeac1acb7834c50a49f9710c3607625'
+    data_loader = DataLoader(trv_api_key, 1)
+    train_announcements = data_loader.get_train_announcement_trv()
+    
     print(f'Announcement 1: {json.dumps(train_announcements[0], indent=2, ensure_ascii=False)}')
     
