7af757606b54429e7c303124c65d5446ac7fa706
Ziaul Chowdhury <romel99@gmail.com>
2024-03-21 18:03:54 +0100

    Updated app with real implementation

--- data-engineering/src/app.py
+++ data-engineering/src/app.py
@@ -1,5 +1,30 @@
-import time
+from data_loader import DataLoader
+from data_preprocessor import DataPreprocessor
+from train_delay_classifier_model import TrainDelayClassifier
+from pickle import dump, load
+import json
 
-t = time.localtime()
-current_time = time.strftime("%H:%M:%S", t)
-print(current_time)
+def train_test_model(trv_api_key, num_days):
+    print('Loading data ...')
+    data_loader = DataLoader(trv_api_key, num_days)
+    train_announcements = data_loader.get_train_announcement_trv()
+    print(f'DONE! \nAnnouncement: {json.dumps(train_announcements[0], indent=2, ensure_ascii=False)}')
+    
+    print('Preprocessing data ...')
+    data_processor = DataPreprocessor(train_announcements)
+    
+    print('Started training and predicting train delays ...')
+    trainDelayClassifier = TrainDelayClassifier(data_processor)
+    print('Done, prediction!')
+    return trainDelayClassifier
+
+def dump_model_weights(trainDelayClassifier):
+    print('Dumping model weights to pkl file ...')
+    dump(trainDelayClassifier, open('train_delay_classifier_weights.pkl', 'wb'))
+    
+if __name__ == "__main__":
+    trv_api_key = 'adeac1acb7834c50a49f9710c3607625'
+    num_days = 2
+    clf = train_test_model(trv_api_key, num_days)
+    
+    dump_model_weights(clf)
