393723e8ca67299481c5042109c7e7cf92881caf
Ziaul Chowdhury <ziaul.chowdhury@tipico.com>
2024-05-18 00:09:00 +0200

    Update init_test.py

--- data-engineering/tests/init_test.py
+++ data-engineering/tests/init_test.py
@@ -1,14 +1,23 @@
+import sys
+sys.path.append("src")
+import json
+from data_loader import DataLoader
+import pytest
+
 def test_square():
     assert True, 'Test passed'
 
-def test_data_preprocessor():
-    from data_preprocessor import DataPreprocessor
-    import json
-    from data_loader import DataLoader
-
+'''
+@pytest.mark.parametrize("num_days,expected_result", [(2, True), (1, True), (3, False)])
+def test_data_loader_two_days(num_days, expected_result):
     trv_api_key = 'adeac1acb7834c50a49f9710c3607625'
     num_days = 2
     data_loader = DataLoader(trv_api_key, num_days)
     train_announcements = data_loader.get_train_announcement_trv()
     print(f'Announcement 1: {json.dumps(train_announcements[0], indent=2, ensure_ascii=False)}')
-    assert len(train_announcements) > 0, 'Test passed'
+
+    if expected_result:
+        assert len(train_announcements) > 0, 'Test passed'
+    else:
+        assert len(train_announcements) == 0, 'Test passed'
+'''
